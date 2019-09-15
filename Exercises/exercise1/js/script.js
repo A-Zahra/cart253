f05d64d272e4cc81fa245b10de823740b35ab116
A-Zahra <z.a.ahmadi18@gmail.com>
2019-09-15 11:52:10 -0400

    Text that follows mouse location
    Ex1: Added text under that follows the mouse

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -20,27 +20,34 @@ let emojisImage;
 let emojisX;
 let emojisY;
 let emojisSize = 100;
-// preload()
 
+// Declaring the text variable and assigning text to it + declaring x and y variables to assign the current position of the text
+let hello = "Hello I am here!!";
+let helloX;
+let helloY;
+
+// preload()
 // Displaying the image at the start of the program before anything else happens
 function preload() {
   emojisImage = loadImage("assets/images/ewordemojis.png");
 }
-// setup()
-//
-// Set up the canvas, position the images, set the image mode.
 
+// setup()
+// Set up the canvas, position the images, set the image mode.
 function setup() {
   // Create our canvas
   createCanvas(640,640);
+
   // Start the image off screen to the bottom left
   // We divide the size by two because we're drawing from the center
   emojisX = -emojisSize/2;
   emojisY = height + emojisSize/2;
+
   // Start the circle off screen to the bottom left
   // We divide the size by two because we're drawing from the center
   circleX = -circleSize/2;
   circleY = height + circleSize/2;
+
   // Start the square off screen to the bottom right
   // We divide the size by two because we're drawing from the center
   squareX = width + squareSize/2;
@@ -54,18 +61,26 @@ function setup() {
   imageMode(CENTER);
 }
 
-
 // draw()
 //
 // Change the circle and square's positions so they move
 // Draw the circle and square on screen
-
+// Draw image on screen and change it's position so it moves
+// Draw text at the current position of the mouse
 function draw() {
   // We don't fill the background so we get a drawing effect
+  // display text at the current mouse location.
+  helloX = mouseX;
+  helloY = mouseY;
+  fill(0,24,0);
+  textSize(20);
+  text (hello, helloX, helloY);
+
   // Move image up and to the right
   emojisX += 1;
   emojisY -= 1;
   image (emojisImage, emojisX, emojisY, emojisSize, emojisSize);
+
   // Move circle up and to the right
   circleX += 1;
   circleY -= 1;
