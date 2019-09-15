109569c8bf2ea189b2d34758d7d4df3c18e0baa0
A-Zahra <z.a.ahmadi18@gmail.com>
2019-09-15 13:40:38 -0400

    Big moving circle
    Ex1:Added the big moving circle at the background

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -1,14 +1,20 @@
 // Exercise 1 - Movement
-// Pippin Barr
-//
+// Prof: Pippin Barr
+// Name: Zahra Ahmadi
+
 // Starter code for exercise 1.
 // Draws a moving square and circle that intersect
 // in the middle of the canvas.
+// Draws a big circle at the background
+// Draws a moving image(emojis) over behind the red circle
+// Draws a text that follows the mouse location
+
 
 // The current position and size of the circle
 let circleX;
 let circleY;
 let circleSize = 100;
+let circleOpacity;
 
 // The current position and size of the square
 let squareX;
@@ -25,6 +31,12 @@ let emojisSize = 100;
 let hello = "Hello I am here!!";
 let helloX;
 let helloY;
+
+// Diclaring the big circle position, size and opacity variables
+let shapeX;
+let shapeY;
+let shapeOpacity;
+let shapeSize = 640;
 
 // preload()
 // Displaying the image at the start of the program before anything else happens
@@ -47,6 +59,7 @@ function setup() {
   // We divide the size by two because we're drawing from the center
   circleX = -circleSize/2;
   circleY = height + circleSize/2;
+  circleOpacity = 100;
 
   // Start the square off screen to the bottom right
   // We divide the size by two because we're drawing from the center
@@ -59,6 +72,13 @@ function setup() {
   noStroke();
   // We'll draw image from the center
   imageMode(CENTER);
+
+  // We'll draw the big circle from the center bottom and set the opacity to zero
+  ellipseMode(CENTER);
+  shapeX = shapeSize/2;
+  shapeY = (height/2) + shapeSize;
+  shapeOpacity = 0;
+
 }
 
 // draw()
@@ -67,8 +87,16 @@ function setup() {
 // Draw the circle and square on screen
 // Draw image on screen and change it's position so it moves
 // Draw text at the current position of the mouse
+// Draw a big circle at the background that covers the previous images, squares, circls and texts.
 function draw() {
   // We don't fill the background so we get a drawing effect
+
+  // Display and changing the big circle speed and opacity
+  shapeY -= 1.2;
+  shapeOpacity += 0.5;
+  fill(86,176,182,shapeOpacity);
+  ellipse(shapeX, shapeY, shapeSize, shapeSize);
+
   // display text at the current mouse location.
   helloX = mouseX;
   helloY = mouseY;
@@ -76,24 +104,26 @@ function draw() {
   textSize(20);
   text (hello, helloX, helloY);
 
+  // Move circle up and to the right
+  // Make the circle transparent red gradually
+  circleX += 0.9;
+  circleY -= 0.9;
+  circleOpacity -= 0.2;
+
   // Move image up and to the right
-  emojisX += 1;
-  emojisY -= 1;
+  emojisX += 0.9;
+  emojisY -= 0.9;
   image (emojisImage, emojisX, emojisY, emojisSize, emojisSize);
 
-  // Move circle up and to the right
-  circleX += 1;
-  circleY -= 1;
-  // Make the circle transparent red
-  fill(255,0,0,10);
+  fill(255,0,0,circleOpacity);
   // Display the circle
   ellipse(circleX,circleY,circleSize,circleSize);
 
   // Move square up and to the left
-  squareX -= 1;
-  squareY -= 1;
+  squareX -= 0.9;
+  squareY -= 0.9;
   // Make the square transparent blue
-  fill(0,0,255,10);
+  fill(0,0,255,90);
   // Display the square
   rect(squareX,squareY,squareSize,squareSize);
 }
