4be2d9f51884627facddf3ef5ba1f144234638c4
A-Zahra <z.a.ahmadi18@gmail.com>
2019-10-25 11:06:01 -0400

    Ex6: fixed 19
    Wrong way of multiplicatin. I replaced the string with a number.

--- Exercises/exercise6/js/Prey.js
+++ Exercises/exercise6/js/Prey.js
@@ -90,7 +90,8 @@ class Prey {
       noStroke();
       fill(this.fillColor);
       this.radius = this.health;
-      ellipse(this.x, this.y, this.radius * "two");
+      // Wrong way of multiplicatin. I replaced the string with a number.
+      ellipse(this.x, this.y, this.radius * 2);
       pop();
     }
 
