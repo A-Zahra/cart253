bd825ced0852185abde5051dbfa0e8fc36dec815
A-Zahra <z.a.ahmadi18@gmail.com>
2019-11-17 18:58:41 -0500

    Ex8: Started exercise 8
    I added transition screen and second step.

---
+++ Exercises/exercise8/js/BarrierStraight.js
@@ -0,0 +1,24 @@
+// Barrier
+//
+// A class that represents barrier
+class BarrierStraight {
+
+  // Constructor
+  //
+  // Sets the initial values for the barrier's properties
+  // Either sets default values or uses the arguments provided.
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.plW = 60;
+    this.plH = 20;
+  }
+
+  // Display barrier
+  display() {
+    fill(255, 76, 39);
+    rectMode(CORNER);
+    rect(this.x, this.y, this.plW, this.plH);
+  }
+
+}
